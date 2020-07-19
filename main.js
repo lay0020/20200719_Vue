d0421e49c062b0bb78fc0eab5763d10ca9850e78
lay0020 <lay0020@gmail.com>
2020-07-19 12:04:47 +0800

    指定元素

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log(我是JS);
+console.log("我是JS");
+
+// 定義渲染區域
+// 變數 Var = new Vue(); - 要新增一個Vue物件
+// el 指定元素 "選取器"
+
+var vue1 = new Vue({
+    el:"#vue1",
+    data: {
+        text: "Hello! this is Vue"
+    }
+});
